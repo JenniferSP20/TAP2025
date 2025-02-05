e5c6029bca939f683bc2cd593ecbf8538154028b
Jennifer Silva Paniagua <jenny.silvap5@gmail.com>
2025-02-05 12:50:37 -0600

    Clase 1

--- src/main/java/com/example/tap2025/HelloApplication.java
+++ src/main/java/com/example/tap2025/HelloApplication.java
@@ -2,22 +2,45 @@ package com.example.tap2025;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+//Pantalla de inicio
+    private VBox vBox;
+    private MenuBar mnbPrincipal;
+    private Menu menCompetencia1, menCompetencia2;
+    private MenuItem mitCalculadora;
+    private Scene escena;
+
+    void CrearUI(){
+        mitCalculadora=new MenuItem("Calculadora");
+        menCompetencia1=new Menu("Competencia1");
+        menCompetencia1.getItems().add(mitCalculadora);
+        mnbPrincipal=new MenuBar();
+        mnbPrincipal.getMenus().add(menCompetencia1);
+
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+
+        vBox= new VBox();
+        stage.setTitle("Hola Mundo de Eventos :)");
+        stage.setScene(new Scene vBox);
         stage.show();
+        stage.setMaximized(true);
     }
 
     public static void main(String[] args) {
-        launch();
+
     }
 }
